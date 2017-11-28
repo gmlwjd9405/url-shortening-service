46f468c62f4134d6b0ee7933dc357e11d7bc1edb
kwonheejeong <gmlwjd9405@naver.com>
2017-11-28 19:17:42 +0900

    Add ShortenerURL model

--- urlshortening/shortener/admin.py
+++ urlshortening/shortener/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ShortenerURL
+
+admin.site.register(ShortenerURL)
--- urlshortening/shortener/models.py
+++ urlshortening/shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class ShortenerURL(models.Model):
+    url = models.CharField(max_length=220, )
+
+    def __str__(self):
+        return str(self.url)
